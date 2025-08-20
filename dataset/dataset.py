4d81934b7b711a44fdab6b3f36e5f25c6ae20f58
MingyuJang <mingyu.jang.dev@gmail.com>
2025-08-20 22:35:21 +0900

    데이터셋 폴더를 수정했습니다.

---
+++ dataset/dataset.py
@@ -0,0 +1,19 @@
+from torch.uitls.data import Dataset, DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize
+from torchvision.datasets import VOCSegmentation
+
+class VOCSegmentationDataset(Dataset):
+    def __init__(self, root='./data', year='2012', image_set='train', transform=None):
+        self.dataset = VOCSegmentation(root=root,year=year,image_set=image_set,download=True)
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self,idx):
+        image, mask = self.dataset[idx]
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+        
+        return image, mask
